6805eb267122352908ad1addcdfa7a973310c75f
Camilo Leonel Amadio <camilo.amadio@microchip.com>
2025-09-19 17:36:11 -0300

    The agent is working when accepting and rejecting the tool call, now I need to integrate with gradio

--- 05_graph_with_pause/agent.py
+++ 05_graph_with_pause/agent.py
@@ -59,18 +59,20 @@ def tools_condition(
     return "__end__"
 
 def tool_calling_permission_edge(state: State) -> Literal["reject_tool_call_node","tools"]:
-    if state["accepted_tool_call"] == "n":
-        return "reject_tool_call_node"
+    if state["accepted_tool_call"] == "y":
+        return "tools"
     
-    return "tools"
+    return "reject_tool_call_node"
+    
+
 
 def reject_tool_call_node(state: State):
     # Is returning the reject, this should be splitted later
     return {"messages": {
                 'role': 'tool',
-                'name': 'tavily_search',
+                'name': state["messages"][-1].tool_calls[0]["name"],
                 'tool_call_id': state["messages"][-1].tool_calls[0]["id"],
-                'content': 'Tool call not allowed, no permission granted by the user.'
+                'content': f'tool call rejected: {state["accepted_tool_call"]}'
             }}
 
 def tool_calling_permission_node(state: State):
@@ -95,16 +97,19 @@ graph_builder.add_edge("reject_tool_call_node", "chatbot")
 graph_builder.add_edge("tools", "chatbot")
 
 checkpointer = InMemorySaver()
-graph = graph_builder.compile(checkpointer=checkpointer)
+graph = graph_builder.compile(
+#    checkpointer=checkpointer
+)
 
 # The compiled graph is something like this
 # https://mermaid.live/edit#pako:eNqNUsGO2yAQ_RU0vSSS7brYJDFZ5dJ8wp66rixig01LwMK43W2Uf19MUrerbKOeYJj33rwZ5gS1aThQiOO41LXRQra01AgJZX7WHbMuRAjVo_3BKVJSc2ZLHeCtZX2HHvfbUpeuqgbn4VW1eHrod3P08LHffV1SSoW0g5uAXtUdjFtcz-X05oxRVc2Ul2-rntujHAZpdKW9ucW9ZGBb_o3XrppxF9q7r3O1IcgOy4t1rpvZeLjPthW7uJ4bQnG8Q1fv27_6QXHiE1f6beJeE9t_NnFT7J7MpdC7Ov_JDSOZscNtq34aw54L1HDBRuWQkErRDwKLVIho2o2447LtHP2U4DeE8PsBHpue1dK90PQNYJrzVe4gDitRQwStlQ1QZ0cewdH7ZVMIp2khS3AdP_ISqL82zH4vodRnz-mZ_mLM8TfNmrHtgAqmBh-NfcMc30vmN_cPxP8Yt5_NqB1QEhSAnuAZKN7kCcnX6Wa9zrKc4E0EL0ALkmRFvkpxSoocF8U5gl-hYpqQFVmnKc5wscGrjJDzK5IDMEw
 
 config = RunnableConfig({"configurable": {"thread_id": uuid.uuid4()}})
 
-async def run_agent(message: str, _history = []):
+async def run_agent(message: str, comand: Command | None = None):
     yielded_response = ""
-    async for async_stream_response in graph.astream({"messages": [message]}, config):
+    stream_message = State({"messages": [message], "accepted_tool_call": None}) if comand is None else comand
+    async for async_stream_response in graph.astream(stream_message, config):
         if "chatbot" in async_stream_response:
             tool_calls = async_stream_response["chatbot"]["messages"][0].tool_calls
             if len(tool_calls):
@@ -124,22 +129,34 @@ async def run_agent(message: str, _history = []):
         interrupt_condition = ("__interrupt__" in async_stream_response) and (type(async_stream_response["__interrupt__"]) == tuple) and (len(async_stream_response["__interrupt__"]) == 1) and (type(async_stream_response["__interrupt__"][0]) == langgraph.types.Interrupt)
         if interrupt_condition:
             yielded_response += async_stream_response["__interrupt__"][0].value
+            yielded_response += "\n \n"
 
         if "tool_calling_permission_node" in async_stream_response:
             yielded_response += f"You responded: {async_stream_response['tool_calling_permission_node']['accepted_tool_call']}"
+            yielded_response += "\n \n"
+
+        if "reject_tool_call_node" in async_stream_response:
+            yielded_response += f"Rejected tool calling: {async_stream_response['reject_tool_call_node']['messages']['name']}"
+            yielded_response += "\n \n"
+
 
         yield yielded_response
 
 async def run_agent_with_stop(message:str, history: list):
     """Example of how to consume yielded responses from outside"""
-    is_tool_caling = False
-    async for yielded_data in run_agent(message):
-        yield yielded_data
+    
+    next_node = (await graph.aget_state(config)).next
+    is_human_accepting_tool_call = next_node != ('tool_calling_permission_node',)
+    if is_human_accepting_tool_call:
+        async for yielded_data in run_agent(message):
+            yield yielded_data
+
+    else:
+        command = Command(resume=message)
+        async for yielded_data in run_agent("", command):
+            yield yielded_data
 
 
-#    command = Command(resume=message)
-#    async for yielded_data in graph.astream(command, config):
-#        yield yielded_data
 
 
 async def consume_agent_responses():
@@ -151,7 +168,7 @@ async def consume_agent_responses():
         print("---")
 
 
-    command = Command(resume="some input from a human!!!")
+    command = Command(resume="n")
     async for yielded_data in graph.astream(command, config):
         print("---")
         print(f"{yielded_data}")
@@ -163,5 +180,6 @@ async def consume_agent_responses():
 
 
 
-if __name__ == "__main__":
-    asyncio.run(consume_agent_responses())
+#if __name__ == "__main__":
+# {"messages":"Use the tools to calculate 5+7."}
+#    asyncio.run(consume_agent_responses())
